5026e4bf1e1e04f1bd6f295e950170c457be2a5a
insolor <insolor@gmail.com>
2019-07-06 17:12:05 +0300

    Hide panes on some conditions

--- src/main/java/MusicLendClient/MainWindowController.java
+++ src/main/java/MusicLendClient/MainWindowController.java
@@ -1,6 +1,28 @@
 package MusicLendClient;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+
 public class MainWindowController {
     static String WebserviceURL, access_token;
 
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    private Tab tabOrderInstruments, tabInstrumentsInUse, tabAdmin;
+
+    @FXML
+    void initialize() {
+        // Hide the pane if no instruments in use
+        if(Boolean.TRUE) {
+            tabPane.getTabs().remove(tabInstrumentsInUse);
+        }
+
+        // Hide the pane if the user is not admin
+        if(Boolean.TRUE) {
+            tabPane.getTabs().remove(tabAdmin);
+        }
+    }
 }
