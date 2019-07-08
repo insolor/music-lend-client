9f38d20e0393420e73c018344f9cf380204046af
insolor <insolor@gmail.com>
2019-07-08 14:12:30 +0300

    Add test instrument in dummy shop

--- src/main/java/MusicLendClient/DummyConnection.java
+++ src/main/java/MusicLendClient/DummyConnection.java
@@ -1,13 +1,19 @@
 package MusicLendClient;
 
-public class DummyConnection extends Connection {
-    DummyConnection(String webserviceURL, String userName, String password) {
+import java.math.BigDecimal;
 
+public class DummyConnection extends Connection {
+    private Shop shop;
+
+    DummyConnection(String webserviceURL, String userName, String password) {
+        shop = new Shop();
+        shop.getAvailableInstruments()
+                .add(new Instrument(1, "Test", "Some description", new BigDecimal("100.00")));
     }
 
     @Override
     public Shop getShop() {
-        return new Shop();
+        return shop;
     }
 
     @Override
--- src/main/java/MusicLendClient/OrderInstrumentsController.java
+++ src/main/java/MusicLendClient/OrderInstrumentsController.java
@@ -2,7 +2,6 @@ package MusicLendClient;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
-import java.math.BigDecimal;
 
 public class OrderInstrumentsController {
     private Shop shop;
@@ -13,8 +12,6 @@ public class OrderInstrumentsController {
     @FXML
     void initialize() {
         shop = Main.connection.getShop();
-        shop.getAvailableInstruments()
-                .add(new Instrument(1, "Test", "Some description", new BigDecimal("100.00")));
 
         for (Instrument instrument: shop.getAvailableInstruments()) {
             listAvailableInstruments.getItems().add(instrument.getName());
