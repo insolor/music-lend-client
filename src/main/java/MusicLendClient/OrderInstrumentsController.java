ae448989b2ed6ad78b11abf23ec84ebefe5c0f2a
insolor <insolor@gmail.com>
2019-07-09 13:25:15 +0300

    Use TableView to show list of available instruments

--- src/main/java/MusicLendClient/OrderInstrumentsController.java
+++ src/main/java/MusicLendClient/OrderInstrumentsController.java
@@ -1,20 +1,31 @@
 package MusicLendClient;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.math.BigDecimal;
 
 public class OrderInstrumentsController {
     private Shop shop;
 
     @FXML
-    ListView listAvailableInstruments;
+    TableView<Instrument> tableAvailableInstruments;
 
     @FXML
     void initialize() {
         shop = Main.connection.getShop();
+        TableColumn<Instrument, String> nameColumn = new TableColumn<>("Название");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tableAvailableInstruments.getColumns().add(nameColumn);
+
+        TableColumn<Instrument, BigDecimal> priceForDay = new TableColumn<>("Цена за сутки");
+        priceForDay.setCellValueFactory(new PropertyValueFactory<>("priceForDay"));
+        tableAvailableInstruments.getColumns().add(priceForDay);
 
         for (Instrument instrument: shop.getAvailableInstruments()) {
-            listAvailableInstruments.getItems().add(instrument.getName());
+            tableAvailableInstruments.getItems().add(instrument);
         }
     }
 }
