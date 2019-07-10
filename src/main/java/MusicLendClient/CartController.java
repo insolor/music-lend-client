24d79f106286987a5729a06fc752603b44b4bc22
insolor <insolor@gmail.com>
2019-07-10 12:59:58 +0300

    Add cart window

---
+++ src/main/java/MusicLendClient/CartController.java
@@ -0,0 +1,18 @@
+package MusicLendClient;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+
+public class CartController {
+    @FXML
+    TableView<Instrument> tableInstrumentsInCart;
+
+    @FXML
+    TextArea textDescription;
+
+    @FXML
+    void initialize() {
+
+    }
+}
