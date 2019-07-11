6a1162606910ae717ceeab83fa189718364660cc
insolor <insolor@gmail.com>
2019-07-11 12:22:31 +0300

    Add spinner field in the cart controller

--- src/main/java/MusicLendClient/CartController.java
+++ src/main/java/MusicLendClient/CartController.java
@@ -3,19 +3,20 @@ package MusicLendClient;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.math.BigDecimal;
 
 public class CartController {
+
     private User user;
 
     @FXML
     TextField txtPromo;
+
+    @FXML
+    Spinner spinNumberOfDays;
 
     @FXML
     TableView<Instrument> tableInstrumentsInCart;
