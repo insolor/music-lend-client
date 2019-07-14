5b4fd356856587f06faf9d2db54e5c43f195c4a6
insolor <insolor@gmail.com>
2019-07-14 17:55:07 +0300

    [m] Remove unused import

--- src/main/java/MusicLendClient/CartController.java
+++ src/main/java/MusicLendClient/CartController.java
@@ -1,7 +1,6 @@
 package MusicLendClient;
 
 import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
