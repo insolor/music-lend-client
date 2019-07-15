b4a6d05ce3d431f370b0128c73efb6d433e0f4be
insolor <insolor@gmail.com>
2019-07-15 11:19:45 +0300

    Add combo "Connection type" in the login window

--- src/main/java/MusicLendClient/LoginController.java
+++ src/main/java/MusicLendClient/LoginController.java
@@ -4,27 +4,47 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
+import org.apache.http.conn.HttpHostConnectException;
+
+import java.util.Arrays;
 
 
 public class LoginController {
     static Stage stage;
 
     @FXML
+    ComboBox<String> comboConnectionType;
+
+    @FXML
     private TextField txtUserName, txtPassword, txtWebserviceURL;
 
     @FXML
     private void login() throws Exception {
+        Alert errorAlert = new Alert(AlertType.ERROR);
+
         try {
-            Main.connection = new DummyConnection(txtUserName.getText(), txtPassword.getText());
+            if(comboConnectionType.getValue().equals("REST API")) {
+                Main.connection = new RESTConnection(txtWebserviceURL.getText(), txtUserName.getText(),
+                        txtPassword.getText());
+            }
+            else {
+                Main.connection = new DummyConnection(txtUserName.getText(), txtPassword.getText());
+            }
         }
         catch (Connection.BadUserException badUserException) {
-            Alert alert = new Alert(AlertType.ERROR, "Неправильное имя пользователя или пароль", ButtonType.OK);
-            alert.showAndWait();
+            errorAlert.setContentText("Неправильное имя пользователя или пароль");
+            errorAlert.showAndWait();
+            return;
+        }
+        catch (HttpHostConnectException httpException) {
+            errorAlert.setContentText("Не удалось подключиться к серверу");
+            errorAlert.showAndWait();
             return;
         }
 
@@ -46,5 +66,11 @@ public class LoginController {
     @FXML
     private void cancel() {
         stage.close();
+    }
+
+    @FXML
+    void initialize() {
+        comboConnectionType.getItems().addAll(Arrays.asList("Тестовое подключение", "REST API"));
+        comboConnectionType.setValue("REST API");
     }
 }
