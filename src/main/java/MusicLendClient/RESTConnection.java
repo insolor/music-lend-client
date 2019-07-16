373bf4706c5acebe59751743d6df089581524e73
insolor <insolor@gmail.com>
2019-07-16 10:39:33 +0300

    Pass username and password to the service in json format

--- src/main/java/MusicLendClient/RESTConnection.java
+++ src/main/java/MusicLendClient/RESTConnection.java
@@ -1,5 +1,8 @@
 package MusicLendClient;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
@@ -7,9 +10,7 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.math.BigDecimal;
 import java.util.stream.Collectors;
 
@@ -19,13 +20,26 @@ public class RESTConnection implements Connection {
     private Shop shop;
     private DummyUser user;
 
+    private class UserInfo {
+        @SerializedName("username")
+        String userName;
+        String password;
+
+        UserInfo(String userName, String password) {
+            this.userName = userName;
+            this.password = password;
+        }
+    }
+
     RESTConnection(String webserviceUrl, String userName, String password) throws BadUserException, IOException {
         httpClient = HttpClientBuilder.create().build();
         HttpPost request = new HttpPost(webserviceUrl.concat("/auth"));
         request.addHeader("Content-Type", "application/json");
-        // TODO: make json from real data
-        String jsonString = "{ \"user\": \"Михаил\", \"password\": \"\" }";
-        StringEntity requestEntity = new StringEntity(jsonString, ContentType.APPLICATION_JSON);
+
+        Gson gson = new GsonBuilder().create();
+        String json = gson.toJson(new UserInfo(userName, password));
+
+        StringEntity requestEntity = new StringEntity(json, ContentType.APPLICATION_JSON);
         request.setEntity(requestEntity);
         HttpResponse response = httpClient.execute(request);
 
